490afdad629cb6440f976f54d742b527b3e6d084
AkioMabuchi <mabuchiakio@gmail.com>
2019-08-15 14:42:27 +0900

    Lv27の機能追加

--- Assets/Scripts/Systems/Level27System.cs
+++ Assets/Scripts/Systems/Level27System.cs
@@ -14,7 +14,7 @@ public class Level27System : MonoBehaviour {
 
     PrefabCreator prefabCreator;
 
-    int levelStage = 2;
+    int levelStage = 13;
     int timerCount;
     bool isLevelUp;
 
@@ -58,28 +58,24 @@ public class Level27System : MonoBehaviour {
         switch (levelStage)
         {
             case 0:
-                for (int x = 0; x < 5; x++)
+                for (int i = 0; i < 2; i++)
                 {
-                    for (int y = 0; y < 7; y++)
+                    for (int x = 0; x < 7; x++)
                     {
-                        float positionX1 = x * 50.0f - 400.0f;
-                        float positionX2 = x * 50.0f - 100.0f;
-                        float positionX3 = x * 50.0f + 200.0f;
-                        float positionY = y * 20.0f + 20.0f;
-                        int colorCode = 6 - y;
-                        if (x == 2 && y == 3)
+                        for (int y = 0; y < 4; y++)
                         {
-                            prefabCreator.CreateItemBlock(positionX1, positionY, 8);
-                            prefabCreator.CreateItemBlock(positionX2, positionY, 9);
-                            prefabCreator.CreateItemBlock(positionX3, positionY, 8);
-                        }
-                        else
-                        {
-                            prefabCreator.CreateNormalBlock(positionX1, positionY, colorCode);
-                            prefabCreator.CreateNormalBlock(positionX2, positionY, colorCode);
-                            prefabCreator.CreateNormalBlock(positionX3, positionY, colorCode);
+                            float positionX = x * 50.0f + i * 450.0f - 375.0f;
+                            float positionY = y * 20.0f + 122.0f;
+                            int colorCode = x;
+                            prefabCreator.CreateNormalBlock(positionX, positionY, colorCode);
                         }
                     }
+                }
+                for (int x = 0; x < 3; x++)
+                {
+                    float positionX = x * 450.0f - 450.0f;
+                    float positionY = 182.0f;
+                    prefabCreator.CreateItemBlock(positionX, positionY, 8);
                 }
                 break;
             case 1:
@@ -97,14 +93,54 @@ public class Level27System : MonoBehaviour {
                 }
                 break;
             case 2:
+                int baseColorCodeForPhase2 = Random.Range(0, 7);
+                for (int x = 0; x < 50; x++)
+                {
+                    for (int y = 0; y < 3; y++)
+                    {
+                        float positionX = x * 20.0f - 490.0f;
+                        float positionY = y * 20.0f + 142.0f;
+                        int colorCode = (baseColorCodeForPhase2 + x + 7 - y) % 7;
+                        prefabCreator.CreateSmallBlock(positionX, positionY, colorCode);
+                    }
+                }
                 break;
             case 3:
+                for (int x = 0; x < 7; x++)
+                {
+                    for (int y = 0; y < 3; y++)
+                    {
+                        float positionX = x * 140.0f - 420.0f;
+                        float positionY = y * 20.0f + 142.0f;
+                        int colorCode = x;
+                        prefabCreator.CreateCountBlock(positionX, positionY, colorCode, 10);
+                    }
+                }
+                for (int x = 0; x < 6; x++)
+                {
+                    float positionX = x * 140.0f - 350.0f;
+                    float positionY = 182.0f;
+                    prefabCreator.CreateItemBlock(positionX, positionY, 14);
+                }
                 break;
             case 4:
                 break;
             case 5:
                 break;
             case 6:
+                for (int x = 0; x < 6; x++)
+                {
+                    float positionX = x * 140.0f - 350.0f;
+                    float positionY = 140.0f;
+                    prefabCreator.CreateFlashBlock(positionX, positionY);
+                }
+                for (int x = 0; x < 7; x++)
+                {
+                    float positionX = x * 140.0f - 420.0f;
+                    float positionY = 140.0f;
+                    int itemCode = x % 2 == 0 ? 11 : 9;
+                    prefabCreator.CreateItemBlock(positionX, positionY, itemCode);
+                }
                 break;
             case 7:
                 break;
@@ -119,6 +155,12 @@ public class Level27System : MonoBehaviour {
             case 12:
                 break;
             case 13:
+                for (int x = 0; x < 3; x++)
+                {
+                    float positionX = x * 300.0f - 300.0f;
+                    float positionY = 182.0f;
+                    prefabCreator.CreateItemBlock(positionX, positionY, 12);
+                }
                 break;
             case 14:
                 prefabCreator.CreateSilverBlock(0.0f, 100.0f);
